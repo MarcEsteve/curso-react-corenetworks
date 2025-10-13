49b45926f630c7a6b0cc3728231c72e58290a03e
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-10-13 15:18:18 +0200

    Update main.jsx

--- 01-inicial/src/main.jsx
+++ 01-inicial/src/main.jsx
@@ -1,3 +1,5 @@
+import './basico/01-template-string';
+
 console.log("Hola mundo");
 // Si editas, guardas y recargas, verás el cambio en la consola 
 // Gracias al Live Reload Server de Vite se hace automáticamente
