7d64c658bbcfda04e8eac238190e110d04a57a1e
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-10-14 09:04:10 +0200

    Más ejemplos React

---
+++ 03-react-inicial/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    // Fragment <></> <React.Fragment></React.Fragment>
+    <>
+      <h1>Hola mundo</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus culpa, doloremque omnis voluptates hic impedit sit deserunt! Adipisci doloribus sequi aliquam id ut mollitia, aliquid sint doloremque accusantium animi cupiditate?</p>
+    </>
+  )
+}
+
+export default App
